34a603b7c4ddd9db8364839c9f178bf48a20f811
Minhajul Islam <36797565+minhajul131@users.noreply.github.com>
2022-06-02 23:02:22 +0600

    style and foreach

--- find.js
+++ find.js
@@ -4,22 +4,47 @@ fetch('https://restcountries.com/v2/all')
 
 const displayCountries = countries =>{
     const div  = document.getElementById('countryDisplay')
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
+    
+    countries.forEach(country => {
         const countryDiv = document.createElement('div')
+        countryDiv.className = 'country'
 
-        const name = document.createElement('h3')
-        name.innerText = country.name
-        countryDiv.appendChild(name)
-
-        const capital = document.createElement('p')
-        capital.innerText = country.capital
-        countryDiv.appendChild(capital)
-        
+        const countryInfo = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+        `
+        countryDiv.innerHTML = countryInfo
         div.appendChild(countryDiv)
+    });
 
-        // li.innerText = country.name
-        // ul.appendChild(li)
-        // // console.log(country.name)
-    }
+
+
+
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement('div')
+    //     countryDiv.className = 'country'
+
+    //     const countryInfo = `
+    //         <h3>${country.name}</h3>
+    //         <p>${country.capital}</p>
+    //     `
+    //     countryDiv.innerHTML = countryInfo
+    //     div.appendChild(countryDiv)
+
+
+        // const name = document.createElement('h3')
+        // name.innerText = country.name
+        // countryDiv.appendChild(name)
+
+        // const capital = document.createElement('p')
+        // capital.innerText = country.capital
+        // countryDiv.appendChild(capital)
+
+        // div.appendChild(countryDiv)
+
+        // // li.innerText = country.name
+        // // ul.appendChild(li)
+        // // // console.log(country.name)
+    // }
 }
